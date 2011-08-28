665bf119b68ab3272afc4dcf08561a3a799dd1cd
Óscar González Fernández <ogfernandez@gmail.com>
2011-08-28 18:09:36 +0200

    Add paredit bindings not conflicting with workspace management

--- oscar/init.el
+++ oscar/init.el
@@ -1,4 +1,10 @@
 (global-set-key (kbd "C-.") 'hippie-expand)
 
+(eval-after-load 'paredit
+  '(progn
+    (define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-backward-slurp-sexp)
+    (define-key paredit-mode-map (kbd "C-S-<right>") 'paredit-backward-barf-sexp)))
+
+
 (server-start)
 (load-theme 'zenburn)
