ecdc3349b6fde0e2ce3c1176175acf275f9e6c8f
Óscar González Fernández <ogfernandez@gmail.com>
2012-09-10 18:59:52 +0200

    Add undo-tree-mode and more friendly undo bindings

--- oscar/init.el
+++ oscar/init.el
@@ -5,6 +5,10 @@
     (define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-backward-slurp-sexp)
     (define-key paredit-mode-map (kbd "C-S-<right>") 'paredit-backward-barf-sexp)))
 
+(require 'undo-tree)
+(global-undo-tree-mode)
+(global-set-key (kbd "C--") 'undo-tree-undo)
+
 
 (server-start)
 (load-theme 'zenburn)
