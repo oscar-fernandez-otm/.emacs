7a2a9ba05f3a0d0785b15310977a0190ba798687
Óscar González Fernández <ogfernandez@gmail.com>
2013-05-02 19:37:00 +0200

    Add multiple cursors

--- oscar/init.el
+++ oscar/init.el
@@ -34,3 +34,9 @@
   '(progn
      (require 'auto-complete-config)
      (ac-config-default)))
+
+;; multiple cursors
+(global-set-key (kbd "C-c C-m") 'mc/edit-lines)
+(global-set-key (kbd "C-:") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-;") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-:") 'mc/mark-all-like-this)
