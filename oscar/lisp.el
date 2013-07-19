8d9a70b33d27240655631a934be74fb749136b0a
Oscar Gonzalez Fernandez <ogfernandez@gmail.com>
2013-07-19 13:36:01 +0200

    Configure ac-nrepl

--- oscar/lisp.el
+++ oscar/lisp.el
@@ -1,14 +1,24 @@
-(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
+(add-hook 'after-init-hook 'lisp-customizations)
 
-(define-key lisp-mode-shared-map (kbd "<C-tab>") 'lisp-complete-symbol)
+(defun lisp-customizations ()
 
-(add-hook 'nrepl-mode-hook
-          (lambda ()
-            (define-key nrepl-interaction-mode-map (kbd "C-,")
-              'nrepl-indent-and-complete-symbol)))
+  (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
+
+  (define-key lisp-mode-shared-map (kbd "<C-tab>") 'lisp-complete-symbol)
+
+  (require 'ac-nrepl)
+  (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'nrepl-mode))
+
+  (add-hook 'nrepl-mode-hook
+            (lambda ()
+              (define-key nrepl-interaction-mode-map (kbd "C-,")
+                'nrepl-indent-and-complete-symbol)))
 
 
-(add-hook 'nrepl-mode-hook 'paredit-mode)
+  (add-hook 'nrepl-mode-hook 'paredit-mode)
 
-(add-hook 'nrepl-interaction-mode-hook
-  'nrepl-turn-on-eldoc-mode)
+  (add-hook 'nrepl-interaction-mode-hook
+            'nrepl-turn-on-eldoc-mode))
