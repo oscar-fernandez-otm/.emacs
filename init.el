4ab65f24b1026db1bd44a1de67981a1f9d3d7be4
Oscar Gonzalez Fernandez <ogfernandez@gmail.com>
2014-02-16 16:59:29 +0100

    Migrate to cider

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby starter-kit-js zenburn-theme undo-tree nrepl auto-complete ac-nrepl multiple-cursors expand-region iy-go-to-char key-chord ace-jump-mode multi-term ag)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby starter-kit-js zenburn-theme undo-tree cider auto-complete ac-nrepl multiple-cursors expand-region iy-go-to-char key-chord ace-jump-mode multi-term ag)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
--- oscar/lisp.el
+++ oscar/lisp.el
@@ -4,21 +4,21 @@
 
   (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
-  (define-key lisp-mode-shared-map (kbd "<C-tab>") 'lisp-complete-symbol)
+  (define-key lisp-mode-shared-map (kbd "C-,") 'lisp-complete-symbol)
 
   (require 'ac-nrepl)
-  (add-hook 'nrepl-repl-mode-hook 'ac-nrepl-setup)
-  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-interaction-mode-hook 'ac-nrepl-setup)
   (eval-after-load "auto-complete"
-    '(add-to-list 'ac-modes 'nrepl-mode))
+    '(add-to-list 'ac-modes 'cider-mode))
 
-  (add-hook 'nrepl-interaction-mode-hook
+  (add-hook 'cider-mode-hook
             (lambda ()
-              (define-key nrepl-interaction-mode-map (kbd "C-,")
-                'nrepl-indent-and-complete-symbol)))
+              (define-key cider-mode-map (kbd "C-,")
+                'complete-symbol)))
 
 
-  (add-hook 'nrepl-repl-mode-hook 'paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
-  (add-hook 'nrepl-interaction-mode-hook
-            'nrepl-turn-on-eldoc-mode))
+  (add-hook 'cider-mode-hook
+            'cider-turn-on-eldoc-mode))
