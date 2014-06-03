e30cfea7585b63dd045a891bcbac18a218da4bda
Oscar Gonzalez Fernandez <ogfernandez@gmail.com>
2014-06-03 19:23:08 +0200

    Add melpa stable

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (when (not package-archive-contents)
