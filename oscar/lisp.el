b51c6aa61537e27a0571829c6e3e49921050c7d7
Oscar Gonzalez Fernandez <ogfernandez@gmail.com>
2014-08-20 10:07:17 +0100

    Add function for jumping to a namespace

--- oscar/lisp.el
+++ oscar/lisp.el
@@ -1,5 +1,20 @@
 (add-hook 'after-init-hook 'lisp-customizations)
 
+(defun custom/jump-to-namespace(regexp)
+  (interactive (list (format "\\(ns.*%s$" (read-from-minibuffer "Jump to namespace: "))))
+  (let ((b (ag-project-regexp regexp))
+        (i 0))
+    (switch-to-buffer b)
+    (beginning-of-buffer)
+    (while (and (not (search-forward "Ag finished" nil t))
+                (< i 1000))
+      (setq i (+ i 1))
+      (sit-for 0.01))
+    (beginning-of-buffer)
+    (compilation-next-error 1)
+    (compile-goto-error)
+    (kill-buffer b)))
+
 (defun lisp-customizations ()
 
   (add-to-list 'auto-mode-alist '("\\.clj.\\'" . clojure-mode))
@@ -7,4 +22,6 @@
   (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
   (add-hook 'cider-mode-hook
-            'cider-turn-on-eldoc-mode))
+            'cider-turn-on-eldoc-mode)
+  
+  (global-set-key (kbd "C-c j") 'custom/jump-to-namespace))
