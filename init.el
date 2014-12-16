9e12df8d2a67ed9a98ffc17b4c5a4c7ba95588fc
Oscar Gonzalez Fernandez <Oscar.Fernandez@agentsmutual.co.uk>
2014-12-16 15:21:37 +0000

    Switch to melpa stable

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
