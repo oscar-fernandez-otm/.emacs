416782a1cdb4dad092bb586b9feb7daaec6b45ba
Oscar Gonzalez Fernandez <oscar.fernandez@agentsmutual.co.uk>
2015-01-28 18:52:48 +0000

    Add clj refactor

--- oscar/lisp.el
+++ oscar/lisp.el
@@ -1,20 +1,3 @@
-(add-hook 'after-init-hook 'lisp-customizations)
-
-(defun custom/jump-to-namespace(regexp)
-  (interactive (list (format "\\(ns.*%s$" (read-from-minibuffer "Jump to namespace: "))))
-  (let ((b (ag-project-regexp regexp))
-        (i 0))
-    (switch-to-buffer b)
-    (beginning-of-buffer)
-    (while (and (not (search-forward "Ag finished" nil t))
-                (< i 1000))
-      (setq i (+ i 1))
-      (sit-for 0.01))
-    (beginning-of-buffer)
-    (compilation-next-error 1)
-    (compile-goto-error)
-    (kill-buffer b)))
-
 (defun lisp-customizations ()
 
   (add-to-list 'auto-mode-alist '("\\.clj.\\'" . clojure-mode))
@@ -23,7 +6,8 @@
 
   (add-hook 'cider-mode-hook
             'cider-turn-on-eldoc-mode)
+  (require 'clj-refactor)
   (add-hook 'clojure-mode-hook (lambda ()
-                                 (define-key clojure-mode-map (kbd "C-c l") 'align-cljlet)))
-
-  (global-set-key (kbd "C-c j") 'custom/jump-to-namespace))
+                                 (define-key clojure-mode-map (kbd "C-c l") 'align-cljlet)
+                                 (clj-refactor-mode 1)
+                                 (cljr-add-keybindings-with-prefix "C-c j"))))
