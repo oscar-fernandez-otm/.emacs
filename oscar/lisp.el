314733f245c5cf6935a3cb14b2a3498398be2891
Oscar Gonzalez Fernandez <oscar.fernandez@agentsmutual.co.uk>
2016-08-12 18:35:52 +0100

    No longer compatible after the upgrade

--- oscar/lisp.el
+++ oscar/lisp.el
@@ -1,11 +1,6 @@
 (defun lisp-customizations ()
-
-  (add-to-list 'auto-mode-alist '("\\.clj.\\'" . clojure-mode))
-
   (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
-  (add-hook 'cider-mode-hook
-            'cider-turn-on-eldoc-mode)
   (require 'clj-refactor)
   (add-hook 'clojure-mode-hook (lambda ()
                                  (define-key clojure-mode-map (kbd "C-c l") 'align-cljlet)
