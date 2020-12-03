9e9e95834b1de4922e1b6670d0d2559fbf68f0f1
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2020-12-03 09:06:58 +0000

    Remove no needed stuff

--- oscar/lisp.el
+++ oscar/lisp.el
@@ -5,6 +5,4 @@
   (add-hook 'clojure-mode-hook (lambda ()
                                  (define-key clojure-mode-map (kbd "C-c l") 'align-cljlet)
                                  (clj-refactor-mode 1)
-                                 (cljr-add-keybindings-with-prefix "C-c j")))
-
-  (setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))"))
+                                 (cljr-add-keybindings-with-prefix "C-c j"))))
