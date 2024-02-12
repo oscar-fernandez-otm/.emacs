f42a0f480bcd0665f5b658c9e07890c130ada841
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2024-02-12 22:54:43 +0000

    Transient example for search/replace
    Not of much use yet.

--- oscar/init.el
+++ oscar/init.el
@@ -252,3 +252,74 @@ If you want to switch in the same window pass a prefix argument."
   :bind
   ("C-c a r" . ag-regexp)
   ("C-c a p" . ag-project-regexp))
+(use-package transient
+  :init
+  (transient-define-prefix cc/isearch-menu ()
+    "isearch Menu"
+    [["Edit Search String"
+      ("e"
+       "Edit the search string (recursive)"
+       isearch-edit-string
+       :transient nil)
+      ("w"
+       "Pull next word or character word from buffer"
+       isearch-yank-word-or-char
+       :transient nil)
+      ("s"
+       "Pull next symbol or character from buffer"
+       isearch-yank-symbol-or-char
+       :transient nil)
+      ("l"
+       "Pull rest of line from buffer"
+       isearch-yank-line
+       :transient nil)
+      ("y"
+       "Pull string from kill ring"
+       isearch-yank-kill
+       :transient nil)
+      ("t"
+       "Pull thing from buffer"
+       isearch-forward-thing-at-point
+       :transient nil)]
+     
+     ["Replace"
+      ("q"
+       "Start ‘query-replace’"
+       isearch-query-replace
+       :if-nil buffer-read-only
+       :transient nil)
+      ("x"
+       "Start ‘query-replace-regexp’"
+       isearch-query-replace-regexp
+       :if-nil buffer-read-only     
+       :transient nil)]]
+    
+    [["Toggle"
+      ("X"
+       "Toggle regexp searching"
+       isearch-toggle-regexp
+       :transient nil)
+      ("S"
+       "Toggle symbol searching"
+       isearch-toggle-symbol
+       :transient nil)
+      ("W"
+       "Toggle word searching"
+       isearch-toggle-word
+       :transient nil)
+      ("F"
+       "Toggle case fold"
+       isearch-toggle-case-fold
+       :transient nil)
+      ("L"
+       "Toggle lax whitespace"
+       isearch-toggle-lax-whitespace
+       :transient nil)]
+     
+     ["Misc"
+      ("o"
+       "occur"
+       isearch-occur
+       :transient nil)]]))
+
+(define-key isearch-mode-map (kbd "<f2>") 'cc/isearch-menu)
