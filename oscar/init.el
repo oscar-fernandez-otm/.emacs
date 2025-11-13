0228e822855d40c1518a09a6581ab3f1730a0caf
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2025-11-13 11:52:40 +0000

    Also tab when doing C-Shift-Return

--- oscar/init.el
+++ oscar/init.el
@@ -108,7 +108,7 @@ If you want to switch in the same window pass a prefix argument."
   ;; help for decoding sequence sent by the terminal in Linux
   (global-set-key (kbd "C-<return>") (kbd "C-e C-j TAB"))
   (define-key input-decode-map "\e[13;6U" [(control shift return)])
-  (global-set-key (kbd "C-S-<return>") (kbd "C-a <return> C-p"))
+  (global-set-key (kbd "C-S-<return>") (kbd "C-a <return> C-p TAB"))
 
   ;; recentf mode
   (recentf-mode t)
